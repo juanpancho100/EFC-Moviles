2e85afc99848250af2e1fd63620eaf0fb6f024b8
juanpancho100 <juanpanchoguti@gmail.com>
2021-01-26 23:38:02 -0500

    final

---
+++ app/src/main/java/com/example/efc_master/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.efc_master;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Siguiente(View view){
+        Intent siguiente = new Intent(this, MainActivity2.class);
+            startActivity(siguiente);
+
+    }
+}
